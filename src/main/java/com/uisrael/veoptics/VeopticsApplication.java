e896b08b36874de73098d832eb23729d2335ba66
Carlos Zuniga <cmzuniga@sudamericano.edu.ec>
2025-12-16 21:25:27 -0500

    base del proyecto

---
+++ src/main/java/com/uisrael/veoptics/VeopticsApplication.java
@@ -0,0 +1,13 @@
+package com.uisrael.veoptics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeopticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeopticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uisrael/veoptics/VeopticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.veoptics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeopticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
