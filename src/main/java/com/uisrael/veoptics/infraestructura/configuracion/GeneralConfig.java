aed4349d80d809648ce37f72ea3a5beae0a984a4
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-11 17:50:53 -0500

    Actualizacion con anexo configuracion para Optometrista

--- src/main/java/com/uisrael/veoptics/infraestructura/configuracion/GeneralConfig.java
+++ src/main/java/com/uisrael/veoptics/infraestructura/configuracion/GeneralConfig.java
@@ -3,10 +3,15 @@ package com.uisrael.veoptics.infraestructura.configuracion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.uisrael.veoptics.aplicacion.casouso.entradas.IOptometristaCasoUso;
 import com.uisrael.veoptics.aplicacion.casouso.entradas.IPacienteCasoUso;
+import com.uisrael.veoptics.aplicacion.casouso.impl.OptometristaCasoUsoImpl;
 import com.uisrael.veoptics.aplicacion.casouso.impl.PacienteCasoUsoImpl;
+import com.uisrael.veoptics.dominio.repositorios.IOptometristaRepositorio;
 import com.uisrael.veoptics.dominio.repositorios.IPacienteRepositorio;
+import com.uisrael.veoptics.infraestructura.persistencia.adaptadores.OptometristaRepositorioImpl;
 import com.uisrael.veoptics.infraestructura.persistencia.adaptadores.PacienteRepositorioImpl;
+import com.uisrael.veoptics.infraestructura.persistencia.mapeadores.IOptometristaJpaMapper;
 import com.uisrael.veoptics.infraestructura.persistencia.mapeadores.IPacienteJpaMapper;
 import com.uisrael.veoptics.infraestructura.repositorios.IPacienteJpaRepositorio;
 
@@ -21,6 +26,16 @@ public class GeneralConfig {
 	@Bean
 	IPacienteCasoUso pacienteCasoUso(IPacienteRepositorio repositorio) {
 		return new PacienteCasoUsoImpl(repositorio);
+	};
+	
+	@Bean
+	IOptometristaRepositorio optometristaRepositorio(IOptometristaJpaRepositorio jpaRepositorio, IOptometristaJpaMapper mapper) {
+		return new OptometristaRepositorioImpl(jpaRepositorio, mapper);
+	};
+
+	@Bean
+	IOptometristaCasoUso optometristaCasoUso(IOptometristaRepositorio repositorio) {
+		return new OptometristaCasoUsoImpl(repositorio);
 	};
 
 }
