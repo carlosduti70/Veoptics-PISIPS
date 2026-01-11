86b3d3206e3eb97425cefaf3100b9ae3e2f6386f
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-11 18:21:26 -0500

    Actualizacion al controlador de Optometrista

--- src/main/java/com/uisrael/veoptics/presentacion/controladores/OptometristaControlador.java
+++ src/main/java/com/uisrael/veoptics/presentacion/controladores/OptometristaControlador.java
@@ -2,6 +2,7 @@ package com.uisrael.veoptics.presentacion.controladores;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,25 +21,28 @@ import jakarta.validation.Valid;
 @RequestMapping("/optometrista")
 public class OptometristaControlador {
 	// dependencias de la arquitectura
-		private final IOptometristaCasoUso optometristaCasoUso;// casos de uso
-		private final IOptometristaDtoMapper mapper;// mapeadores
-
-		public OptometristaControlador(IOptometristaCasoUso optometristaCasoUso, IOptometristaDtoMapper mapper) {
-			super();
-			this.optometristaCasoUso = optometristaCasoUso;
-			this.mapper = mapper;
+	private final IOptometristaCasoUso optometristaCasoUso;// casos de uso
+	private final IOptometristaDtoMapper mapper;// mapeadores
+	
+	public OptometristaControlador(IOptometristaCasoUso optometristaCasoUso, IOptometristaDtoMapper mapper) {
+		
+		super();
+		this.optometristaCasoUso = optometristaCasoUso;
+		this.mapper = mapper;
+		
 		}
-
-		@GetMapping("/listar")
-		public List<OptometristaResponseDTO> listar() {
-			return optometristaCasoUso.listar().stream().map(mapper::toResponseDto).toList();
+	
+	@GetMapping("/listar")
+	public List<OptometristaResponseDTO> listar() {
+		return optometristaCasoUso.listar().stream().map(mapper::toResponseDto).toList();
 		}
 
 		// post
-		@PostMapping("/crear")
-		@ResponseStatus(HttpStatus.CREATED)
-		public OptometristaResponseDTO crear(@Valid @RequestBody OptometristaRequestDTO request) {
-			return mapper.toResponseDto(optometristaCasoUso.crear(mapper.toDomain(request)));
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	
+	public OptometristaResponseDTO crear(@Valid @RequestBody OptometristaRequestDTO request) {
+		return mapper.toResponseDto(optometristaCasoUso.crear(mapper.toDomain(request)));
 		}
 
 }
