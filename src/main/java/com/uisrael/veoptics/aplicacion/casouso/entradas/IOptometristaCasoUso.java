ad8fff30f0fd0b7c6d785d04f490f6049b1a88c6
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-11 15:19:20 -0500

    Actualiza Caso  de uso Optometrista

--- src/main/java/com/uisrael/veoptics/aplicacion/casouso/entradas/IOptometristaCasoUso.java
+++ src/main/java/com/uisrael/veoptics/aplicacion/casouso/entradas/IOptometristaCasoUso.java
@@ -1,5 +1,14 @@
 package com.uisrael.veoptics.aplicacion.casouso.entradas;
 
-public class OptometristaCasoUso implements IOptometristaCasoUso   {
+import java.util.List;
+
+import com.uisrael.veoptics.dominio.entidades.Optometrista;
+
+public interface IOptometristaCasoUso {
+	
+	Optometrista crear(Optometrista optometrista);
+	Optometrista ObtenerPorId(int id);
+	List<Optometrista>listar ();
+	void eliminar(int id);
 
 }
