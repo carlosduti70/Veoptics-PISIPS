8661d4f54586aa45ee41f82d5244fd4fc19f6317
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-11 17:19:06 -0500

    Creacion clase DTO Optometrista

---
+++ src/main/java/com/uisrael/veoptics/presentacion/dto/response/OptometristaResponseDTO.java
@@ -0,0 +1,60 @@
+package com.uisrael.veoptics.presentacion.dto.response;
+
+public class OptometristaResponseDTO {
+	private int idOptometrista;
+	private String nombre;
+	private String apellido;
+	private String ci;
+	private String registroProfesional;
+	private String telefono;
+	private char estado;
+	public int getIdOptometrista() {
+		return idOptometrista;
+	}
+	public void setIdOptometrista(int idOptometrista) {
+		this.idOptometrista = idOptometrista;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getCi() {
+		return ci;
+	}
+	public void setCi(String ci) {
+		this.ci = ci;
+	}
+	public String getRegistroProfesional() {
+		return registroProfesional;
+	}
+	public void setRegistroProfesional(String registroProfesional) {
+		this.registroProfesional = registroProfesional;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public char getEstado() {
+		return estado;
+	}
+	public void setEstado(char estado) {
+		this.estado = estado;
+	}
+	
+	
+	
+
+	
+	
+
+}
