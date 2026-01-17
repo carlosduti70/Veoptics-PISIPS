fcadabad45ba6ec956894075c4acdc4f89fd3969
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-17 09:24:24 -0500

    grego interfaz Examen optometrico

---
+++ src/main/java/com/uisrael/veoptics/dominio/repositorios/IExamenOptometricoRepositorio.java
@@ -0,0 +1,18 @@
+package com.uisrael.veoptics.dominio.repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.uisrael.veoptics.dominio.entidades.ExamenOptometrico;
+
+public interface IExamenOptometricoRepositorio {
+	
+	ExamenOptometrico guardar(ExamenOptometrico examenoptometrico);
+
+	Optional<ExamenOptometrico> buscarPorId(int id);
+
+	List<ExamenOptometrico> listarTodos();
+
+	void eliminar(int id);
+
+}
