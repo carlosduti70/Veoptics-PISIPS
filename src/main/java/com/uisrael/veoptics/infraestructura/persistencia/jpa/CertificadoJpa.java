6e561cc5490c3e51f46208f203bc3444a80f446b
Gloria Fernanda Cumbicos Canar <gloria.cumbicos.canar@estudiantes.cordillera.edu.ec>
2026-01-17 09:35:56 -0500

    Agrego relacion con examen optometrico

--- src/main/java/com/uisrael/veoptics/infraestructura/persistencia/jpa/CertificadoJpa.java
+++ src/main/java/com/uisrael/veoptics/infraestructura/persistencia/jpa/CertificadoJpa.java
@@ -2,11 +2,13 @@ package com.uisrael.veoptics.infraestructura.persistencia.jpa;
 
 import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -25,6 +27,7 @@ public class CertificadoJpa implements Serializable{
 	private String tbl_informacion_certificado;
 	private String tbl_ruta_pdf;
 	
-	
+	@OneToMany(mappedBy = "id_examen")
+	private List<ExamenOptometrico> examenoptometrico;
 
 }
